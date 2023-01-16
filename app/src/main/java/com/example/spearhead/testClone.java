456e518ca0c6cd053a451897a3c8d605fa279f6e
Benjamin <bchua2001@gmail.com>
2023-01-16 08:27:54 +0800

    test

--- app/src/main/java/com/example/spearhead/testClone.java
+++ app/src/main/java/com/example/spearhead/testClone.java
@@ -10,5 +10,6 @@ public class testClone extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_clone);
+
     }
 }
